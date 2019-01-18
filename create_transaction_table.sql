484893c0619c4771ec5bac05ead73f8fac46264b
felix.klemm <felix.klemm@192.168.99.1>
2019-01-18 10:01:06 +0100

    Added Table

---
+++ create_transaction_table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXIST customer_transaction (
+    id         integer,
+    action varchar(100) ,
+    username varchar(100),
+    ts timestamp,
+    customername varchar(400),
+    customeraddress varchar(400),
+    state varchar(100),
+    creation_time   timestamp DEFAULT current_timestamp
+);
