d61bfa41d8b8b38438573cc41dbd85c68ee89fbc
felix.klemm <felix.klemm@CPX-XU4XTDK0R4B.dir.svc.accenture.com>
2019-04-30 16:13:27 +0200

    sht fixed

--- src/main/java/com/accenture/streaming/App.java
+++ src/main/java/com/accenture/streaming/App.java
@@ -108,10 +108,10 @@ public class App {
 								  .add("username", DataTypes.StringType)
 								  .add("ts", DataTypes.StringType)));
 		
-		Dataset<Row> action_list = kafkaEntries.select(from_json(col("value"), struct));
+		Dataset<Row> action_list = kafkaEntries.select(from_json(col("value"), struct).as("output"));
 		
 		Dataset<UserActivity> finalEntries = action_list
-				.selectExpr("payload.after.action", "payload.after.id", "payloady.after.username", "payload.after.ts") // JSON fields we extract
+				.selectExpr("output.payload.after.action", "output.payload.after.id", "output.payloady.after.username", "output.payload.after.ts") // JSON fields we extract
 				.toDF("action", "id", "username", "ts") // map columns to new names
 				.as(Encoders.bean(UserActivity.class)); // make a good old JavaBean out of it
 		
