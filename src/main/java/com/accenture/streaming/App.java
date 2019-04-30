4dd560bd240439ae1059b569ca596aefe2a22937
felix.klemm <felix.klemm@CPX-XU4XTDK0R4B.dir.svc.accenture.com>
2019-04-30 15:02:29 +0200

    Fixed

--- src/main/java/com/accenture/streaming/App.java
+++ src/main/java/com/accenture/streaming/App.java
@@ -1,6 +1,7 @@
 package com.accenture.streaming;
 
 import static org.apache.spark.sql.functions.col;
+import static org.apache.spark.sql.functions.from_json;
 import static org.apache.spark.sql.functions.struct;
 import static org.apache.spark.sql.functions.to_json;
 
@@ -14,7 +15,10 @@ import org.apache.spark.sql.streaming.OutputMode;
 import org.apache.spark.sql.streaming.StreamingQuery;
 import org.apache.spark.sql.streaming.StreamingQueryException;
 import org.apache.spark.sql.streaming.Trigger;
+import org.apache.spark.sql.types.DataTypes;
+import org.apache.spark.sql.types.StructType;
 import org.elasticsearch.hadoop.cfg.ConfigurationOptions;
+
 
 /**
  * Spark Structured Stream Processing with Apache Kafka <br>
@@ -77,14 +81,37 @@ public class App {
 
 		// Definition of the Kafka Stream including the mapping of JSON into Java
 		// Objects
-		Dataset<UserActivity> kafkaEntries = spark.readStream() // read a stream
+//		Dataset<UserActivity> kafkaEntries = spark.readStream() // read a stream
+//				.format("kafka") // from KAFKA
+//				.option("kafka.bootstrap.servers", bootstrapServers) // connection to servers
+//				.option("failOnDataLoss", "false")
+//				.option("subscribe", topics).load() // subscribe & load
+//				.selectExpr("value.payload.after.action", "value.payload.after.id", "value.payloady.after.username", "value.payload.after.ts") // JSON fields we extract
+//				.toDF("action", "id", "username", "ts") // map columns to new names
+//				.as(Encoders.bean(UserActivity.class)); // make a good old JavaBean out of it
+		
+		// read value
+		Dataset<Row> kafkaEntries = spark.readStream() // read a stream
 				.format("kafka") // from KAFKA
 				.option("kafka.bootstrap.servers", bootstrapServers) // connection to servers
 				.option("failOnDataLoss", "false")
 				.option("subscribe", topics).load() // subscribe & load
-				.selectExpr("value.payload.after.action", "value.payload.after.id", "value.payloady.after.username", "value.payload.after.ts") // JSON fields we extract
+				.selectExpr("CAST(value AS STRING)");
+
+		StructType struct = new StructType()
+				  .add("value", new StructType().add("payload", new StructType().add("after", new StructType()				  
+				  .add("action", DataTypes.StringType)
+				  .add("id", DataTypes.StringType)
+				  .add("username", DataTypes.StringType)
+				  .add("ts", DataTypes.StringType))));
+		
+		Dataset<Row> action_list = kafkaEntries.select(from_json(col("value"), struct));
+		
+		Dataset<UserActivity> finalEntries = action_list
+				.selectExpr("payload.after.action", "payload.after.id", "payloady.after.username", "payload.after.ts") // JSON fields we extract
 				.toDF("action", "id", "username", "ts") // map columns to new names
 				.as(Encoders.bean(UserActivity.class)); // make a good old JavaBean out of it
+		
 		
 //		Dataset<UserActivity> kafkaEntries = spark.readStream() // read a stream
 //		.format("kafka") // from KAFKA
